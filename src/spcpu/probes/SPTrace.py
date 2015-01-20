23bcaf600555072eda454eb49bd99d0fd70ee833
Meng Wang <wm8120@gmail.com>
2015-01-20 14:33:09 -0500

    a scratch to trace interval

--- src/spcpu/probes/SPTrace.py
+++ src/spcpu/probes/SPTrace.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2012 ARM Limited
+# Copyright (c) 2012-2014 ARM Limited
 # All rights reserved.
 #
 # The license below extends only to copyright in the software and shall
@@ -9,9 +9,6 @@
 # terms below provided that you ensure that this notice is replicated
 # unmodified and in its entirety in all distributions of the software,
 # modified or unmodified, in source code or in binary form.
-#
-# Copyright (c) 2007 The Regents of The University of Michigan
-# All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -36,49 +33,16 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-# Authors: Nathan Binkert
+# Authors: Curtis Dunham
 
 from m5.params import *
-from BaseSimpleCPU import BaseSimpleCPU
-from SimPoint import SimPoint
+from Probe import ProbeListenerObject
 
-class LivespCPU(BaseSimpleCPU):
-    """Simple CPU model executing a configurable number of
-    instructions per cycle. This model uses the simplified 'atomic'
-    memory mode."""
+class SPTrace(ProbeListenerObject):
+    """Probe for dumping simpoint interval trace."""
 
-    type = 'LivespCPU'
-    cxx_header = "cpu/spcpu/spcpu.hh"
+    type = 'SPTrace'
+    cxx_header = "cpu/spcpu/probes/sptrace.hh"
 
-    @classmethod
-    def memory_mode(cls):
-        return 'atomic'
-
-    @classmethod
-    def support_take_over(cls):
-        return True
-
-    width = Param.Int(1, "CPU width")
-    simulate_data_stalls = Param.Bool(False, "Simulate dcache stall cycles")
-    simulate_inst_stalls = Param.Bool(False, "Simulate icache stall cycles")
-    fastmem = Param.Bool(False, "Access memory directly")
-
-    def addSimPointProbe(self, interval, bbv_file):
-        simpoint = SimPoint()
-        simpoint.interval = interval
-        if bbv_file is not None:
-            simpoint.profile_file = bbv_file
-        self.probeListener = simpoint
-
-    def addSPTraceProbe(self, skip_num, trace_file):
-        sptrace = SPTrace()
-        sptrace.skip_num = skip_num
-        if trace_file is not None:
-            sptrace.trace_file = trace_file
-        self.probeListener = sptrace
-
-    ckpt_insts_any_thread = Param.Counter(0, "take first checkpoint when simulating these instructions")
-
-    @classmethod
-    def export_methods(cls, code):
-        code('''void spInstStop(Counter insts);''')
+    skip_num = Param.UInt64(0, "Skip first skip_num simulated instructions when dumping trace")
+    trace_file = Param.String("sptrace.gz", "simpoint interval trace file")
